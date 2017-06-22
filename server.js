650aa46632bc4653d0518e823431eba3abaa7a21
shubham rai <roy.shubham51@gmail.com>
2017-06-22 16:23:14 +0530

    Database Service added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var pg = require('pg');
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,6 +22,19 @@ app.get('/listUsers', function(req, res) {
        res.end( data );
    });
 })
+
+//Database Service
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
