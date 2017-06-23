15e27732c7d972a72a4c6574785b1b81045ef025
Zeeshan Al Abedin <zeeshanalabedin@gmail.com>
2017-06-23 11:24:13 +0530

    added npm modules

--- server.js
+++ server.js
@@ -30,15 +30,9 @@ app.get('/db', function (request, response) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
-       { 
-           console.error(err); response.send("Error " + err); 
-       }
+       { console.error(err); response.send("Error " + err); }
       else
-       { 
-           response.render('pages/db', {
-               results: result.rows
-           }); 
-       }
+       { response.render('pages/db', { results: result.rows }); }
     });
   });
 });
