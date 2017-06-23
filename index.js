7e6360cb81fdcf14eea04ec5a4e7175cca4301bd
shubham rai <roy.shubham51@gmail.com>
2017-06-23 14:01:50 +0530

    make-it-better

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT info FROM orders', function(err, result) {
       done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+      
     });
   });
 });
