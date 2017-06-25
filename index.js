78d9d27294f24f69f33def16476594d97c46cf74
shubham rai <roy.shubham51@gmail.com>
2017-06-25 10:59:47 +0530

    make-it-better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.send(results); }
     });
   });
 });
