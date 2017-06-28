40d8960116f14fba60877d973b9d6fd0c3c80102
shubham rai <roy.shubham51@gmail.com>
2017-06-28 23:37:58 +0530

    make-it-better

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/userdata',function(request,response){
 
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   	
-  	  client.query('SELECT product_id FROM userdata WHERE name = "shubham" ', function(err, result) {
+  	  client.query('SELECT product_id FROM userdata WHERE name = 'shubham' ', function(err, result) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
