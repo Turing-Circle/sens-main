cf02192b23d3c18286237d7e26ed85c9fc972ed8
shubham rai <roy.shubham51@gmail.com>
2017-06-28 11:29:32 +0530

    make-it-better

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ app.get('/db', function (request, response) {
   });
 });
 
+
+app.get('/userdata', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT username,password FROM userdata', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result); }
+    });
+  });
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
