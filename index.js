2f44b28bb0941020ac2df714de67ef2a49d021c2
shubham rai <roy.shubham51@gmail.com>
2017-06-29 21:18:20 +0530

    make-it-better'

--- index.js
+++ index.js
@@ -48,6 +48,21 @@ app.get('/userdata',function(request,response){
   });
 });
 
+app.get('/sensordata', function (request, response) {
+  var query1 = url.parse(request.url, true);
+	var name = query1.query.pid;
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table WHERE product_id = \''+name +'\' ', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result); }
+    });
+  });
+});
+
 
 
 app.get('/signup',function(request,response){
