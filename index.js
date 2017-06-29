29e13b42e27fd03c4218a823d2074300ee578f93
shubham rai <roy.shubham51@gmail.com>
2017-06-29 11:01:17 +0530

    make-it-better

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table ', function(err, result) {
+    client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
