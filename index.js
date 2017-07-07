75eef612de219f0d827ad7d5d36dbc6406f06740
shubham rai <roy.shubham51@gmail.com>
2017-07-07 16:43:49 +0530

    sabka badla lega re

--- index.js
+++ index.js
@@ -110,7 +110,16 @@ app.get('/forgotPassword', function(request, response) {
 	var query1 = url.parse(request.url, true);
 	var name = query1.query.uname;
 
-	response.render('pages/resetPassword');
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	
+  	  client.query('SELECT email FROM userdata WHERE email = \'' + name +'\' ', function(err, result) {
+  	  	done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/resetPassword'); }
+    });
+  });
 });
 
 
