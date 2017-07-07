8c70a2ebfc0c50295eccabb60172ec683e896df6
shubham rai <roy.shubham51@gmail.com>
2017-07-07 19:43:52 +0530

    finally

--- index.js
+++ index.js
@@ -106,7 +106,20 @@ app.get('/signup',function(request,response){
 
 
 app.get('/forgotPassword', function(request, response) {
-  response.render('pages/resetPassword');
+
+	var query1 = url.parse(request.url, true);
+	var name = query1.query.uname;
+
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	
+  	  client.query('SELECT email FROM userdata WHERE email = \'' + name +'\' ', function(err, result) {
+  	  	done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/resetPassword', {results: result.rows} ); }
+    });
+  });
 });
 
 
