efa60e71d97ec3461ebd20c38e8d41a5ec938103
shubham rai <roy.shubham51@gmail.com>
2017-07-17 11:26:00 +0530

    ok

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.get('/forgotPassword', function(request, response) {
 
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   	
-  	  client.query('SELECT email FROM userdata WHERE email = \'' + name +'\' ', function(err, result) {
+  	  client.query(' SELECT email FROM userdata WHERE email = \''+name +'\' ', function(err, result) {
   	  	done();
       if (err)
        { console.error(err); response.send("Error " + err); }
