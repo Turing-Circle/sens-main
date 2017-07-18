2352d18b988ffd6d034f6d31f8be2738b5f2dbc5
shubham rai <roy.shubham51@gmail.com>
2017-07-18 11:06:26 +0530

    okk

--- index.js
+++ index.js
@@ -63,6 +63,25 @@ app.get('/userdata',function(request,response){
 
 
 
+app.get('/validate',function(request,response){
+	
+	var query1 = url.parse(request.url, true);
+	var name = query1.query.uname;
+	
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	
+  	  client.query('SELECT email FROM userdata WHERE email = \'' + name +'\' ', function(err, result) {
+  	  	done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result); }
+    });
+  });
+});
+
+
+
 app.get('/sensordata', function (request, response) {
   var query1 = url.parse(request.url, true);
 	var name = query1.query.pid;
