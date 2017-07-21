4faa363d0d32ad8f05906754472510f5a4d27b0a
Zeeshan Al Abedin <zeeshanalabedin@gmail.com>
2017-07-21 11:01:56 +0530

    made changes

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.set('view engine', 'ejs');
 //Allow Cross Domain Requests 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
  });
 
