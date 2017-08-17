f1cda9cc9b7a194f40a5c0b7e83d5f3a5b2deb6b
Sanjoth Shaw <sanjothshaw@gmail.com>
2017-08-17 11:46:09 +0530

    adsf

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.send(result); }
+       { response.render('pages/db'); }
     });
   });
 });
